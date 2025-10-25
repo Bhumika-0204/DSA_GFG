40b7f636b0a43031460a758d6eec28abb2a6efb3
Bhumika-0204 <bhumikakumari0802@gmail.com>
2025-10-25 23:09:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/TypeCast And Double It/type-cast-and-double-it.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string num;
+    getline(cin, num);
+    // TypeCast to int double it and print
+    int n=stoi(num);
+    cout<<2*n;
+
+    return 0;
+}
